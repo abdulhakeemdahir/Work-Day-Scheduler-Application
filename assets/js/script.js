5e579e82c613a5bb572906e145756d18d190ccaa
Abdulhakeem <somlutions@gmail.com>
2020-11-08 15:20:07 -0800

    Setting up css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
+// Setting up all Moment JS variables
 var today = moment();
 var currentDate = $("#currentDate").text(
   today.format("[Today is] dddd, MMM DD, YYYY")
 );
 var currentDay = $(".currentDay").text(today.format("dddd"));
+
+var currentHour = today.format("k");
+
+// Setting up conditionals with Data Attributes
+var state = $(".hour");
+var stateData = $(state.getAttribute("data-hour"));
+if (stateData < currentHour) {
+    $(state.setAttribute("class", ".past"));
+}
+state.attr("class","present");
+console.log(state);

a6cfb1e2572b11d82eba941346d44cf31cb9f497
Abdulhakeem <somlutions@gmail.com>
2020-11-08 18:43:45 -0800

    got the color designation for past, present, and future set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,16 @@ var currentHour = parseInt(today.format("k"));
 // Setting up conditionals with Data Attributes
 var formEl = $(".form-control");
 var stateHour = $(".hour");
-var stateData = stateHour.attr("data-hour");
-if (stateData < currentHour) {
-  formEl.css("background-color", "#d3d3d3");
+for (var i = 0; i < 9; i++) {
+    var stateData = $(stateHour[i]).attr("data-hour");
+  if (stateData < currentHour) {
+    $(formEl[i]).css("background-color", "#d3d3d3");
+  } else if (stateData == currentHour) {
+    $(formEl[i]).css("background-color", "#ff6961");
+  } else if (stateData > currentHour) {
+    $(formEl[i]).css("background-color", "#77dd77");
+  }
 }
-else if (stateData = currentHour) {
-  formEl.css("background-color", "#ff6961");
-}
-
-else if (stateData > currentHour) {
-  formEl.css("background-color", "#77dd77");
-}
-
+console.log(formEl[i]);
 console.log(currentHour);
 console.log(stateData);
